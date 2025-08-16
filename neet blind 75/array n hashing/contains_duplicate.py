652a859cce8983771131d959260f46b8e8c3e30f
SreshthaRawat <sreshtha118@gmail.com>
2025-08-16 19:02:34 +0530

    init commit

--- neet blind 75/array n hashing/contains_duplicate.py
+++ neet blind 75/array n hashing/contains_duplicate.py
@@ -1,1 +1,14 @@
 #brute force
+# not much to add just plain brute force
+# with n2 TC so typical TLE error 
+
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] == nums[j]:
+                    return True
+        return False
+    
+#sort approach
+# we first sort there
