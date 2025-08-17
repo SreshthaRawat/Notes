95f6cb09d04ecf9faf4efcebaa0ac996efa565f1
SreshthaRawat <sreshtha118@gmail.com>
2025-08-17 10:26:49 +0530

    neet75

--- neet blind 75/array n hashing/contains_duplicate.py
+++ neet blind 75/array n hashing/contains_duplicate.py
@@ -12,3 +12,44 @@ class Solution:
     
 #sort approach
 # we first sort there
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+
+        n=len(nums)
+        
+        nums.sort()
+
+        for i in range(1,n): #edge case of nums=[0] 
+
+            if nums[i]==nums[i-1]:
+                return True
+        return False
+    
+
+# with o of n we have 2 approaches hashset and hashset length 
+
+#hashset
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        seen = set()
+        for num in nums:
+            if num in seen:
+                return True
+            seen.add(num)
+        return False
+    
+# hashset length
+# here the approach is if the length of nums in set is not equal to 
+# length of nums then we return false because set doesnt contain duplicate 
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+
+        return len(set(nums))!=len(nums)
+
+
+        
+
+
+
+
