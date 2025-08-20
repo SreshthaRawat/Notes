601c9d2ed131aaeaa98bbbc10dd3f8f3f1194564
SreshthaRawat <sreshtha118@gmail.com>
2025-08-20 19:36:06 +0530

    rename of files

--- neet blind 75/array n hashing/validAna.py
+++ neet blind 75/array n hashing/validAna.py
@@ -1,5 +1,13 @@
 # my solution
 
+""" 
+so what basically we're doing in this quetion is that
+we create an empty list with 0 as all 26 elements to rep the alphabet
+so to check for the anagram we fill 1 instead of 0 in both s and t 
+and then we match if the list of s and t have same amount of zero
+if not we return false
+ """
+
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         
